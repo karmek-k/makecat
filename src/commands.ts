1af826589d9029be7212f53c43f655e90bcf8724
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-09-08 19:50:53 +0200

    Implement file building

--- src/commands.ts
+++ src/commands.ts
@@ -1,13 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'yaml';
+import ejs from 'ejs';
 
-export function build() {}
-
-export function init(directory: string = '.') {
+export function init(output: string = 'index.html', directory: string = '.') {
   const defaultConfig = yaml.stringify({
     makecat: {
       template: 'default',
+      output,
       document: {
         title: 'Makecat Document',
         lang: 'en'
@@ -16,4 +16,23 @@ export function init(directory: string = '.') {
   });
 
   fs.writeFileSync(path.join(directory, 'makecat.yml'), defaultConfig);
+}
+
+export async function build(configPath: string = 'makecat.yml') {
+  const configFile = fs.readFileSync(configPath, 'utf-8');
+  const config = yaml.parse(configFile).makecat;
+
+  const templatePath = path.join(
+    __dirname,
+    '..',
+    'templates',
+    `${config.template}.ejs`
+  );
+
+  const rendered = await ejs.renderFile(templatePath, {
+    document: config.document,
+    categories: []
+  });
+
+  fs.writeFileSync(config.output, rendered);
 }
